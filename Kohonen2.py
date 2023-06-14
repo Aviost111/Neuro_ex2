4d400f31c40f6a9d4ad960e27139801f9fb32f74
Aviost111 <Aviostroff@yahoo.com>
2023-06-14 03:31:25 +0300

    .

--- Kohonen2.py
+++ Kohonen2.py
@@ -1,9 +1,18 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+def visualize_som(weights, iteration):
+    plt.figure(figsize=(6, 6))
+    plt.imshow(weights.T, cmap='viridis', origin='lower')
+    plt.title(f'SOM Weights at Iteration {iteration}')
+    plt.colorbar()
+    plt.show()
 
 
 # initialize weights
 def initialize_weights(input_dim, output_dim):
-    return np.random.rand(input_dim, output_dim)
+    return np.random.rand(input_dim, output_dim[0], output_dim[1])
 
 
 # set topological neighborhood parameters
@@ -18,21 +27,31 @@ def learning_rate(initial_lr, iteration, max_iteration):
 
 
 # Kohonen SOM algorithm
-def kohonen_som(inputs, output_dim, max_iteration, initial_lr, initial_radius):
+def kohonen_som(inputs, output_dim, max_iteration, initial_lr, initial_radius, visualize_interval=10):
     input_dim = inputs.shape[1]
     weights = initialize_weights(input_dim, output_dim)
     grid = np.array(list(np.ndindex(output_dim)))
+    total_output_neurons = output_dim[0] * output_dim[1]
     for iteration in range(max_iteration):
         # update learning rate and radius
         lr = learning_rate(initial_lr, iteration, max_iteration)
         radius = initial_radius * np.exp(-iteration / np.log(max_iteration))
         for input_vector in inputs:
             # compute distances
-            distances = np.sum((weights - input_vector) ** 2, axis=1)
+            distances = np.sum((weights - input_vector.reshape(input_dim, 1, 1)) ** 2, axis=0)
             # find the winner neuron
-            winner = np.argmin(distances)
+            winner = np.unravel_index(np.argmin(distances), output_dim)
             # update weights of winner and its neighbors
-            for j in range(output_dim):
-                neighborhood = topological_neighborhood(radius, grid[j], grid)
-                weights[:, j] += lr * neighborhood * (input_vector - weights[:, j])
+            for j in range(total_output_neurons):
+                idx = np.unravel_index(j, output_dim)
+                neighborhood = topological_neighborhood(radius, winner, idx)
+                weights[:, idx[0], idx[1]] += lr * neighborhood * (input_vector - weights[:, idx[0], idx[1]])
+
+        # Visualize the SOM at specific intervals
+        if iteration % visualize_interval == 0:
+            visualize_som(weights, iteration)
+
     return weights
+
+
+
