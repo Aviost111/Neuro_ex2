465728fcf3ede5add530a4de379facec2e282bd1
Aviost111 <Aviostroff@yahoo.com>
2023-06-15 22:01:12 +0300

    .

--- Kohonen.py
+++ Kohonen.py
@@ -1,129 +1,187 @@
-# import numpy as np
-#
-#
-# class KohonenSOM:
-#     def __init__(self, input_dim, map_dim, iterations):
-#         self.input_dim = input_dim
-#         self.map_dim = map_dim
-#         self.iterations = iterations
-#         self.map = np.random.rand(map_dim[0], map_dim[1], input_dim)
-#
-#     def train(self, data):
-#         for t in range(self.iterations):
-#             # Select a random input vector
-#             x = data[np.random.randint(data.shape[0])]
-#
-#             # Compute Euclidean distance between input vector and all nodes in the map
-#             distances = np.linalg.norm(self.map - x, axis=2)
-#
-#             # Find the node with the smallest distance (BMU)
-#             bmu = np.unravel_index(np.argmin(distances), distances.shape)
-#
-#             # Determine topological neighbourhood and its radius
-#             sigma = self.map_dim[0] / 2.0 * np.exp(-t / self.iterations)
-#             neighbourhood = np.exp(
-#                 -np.linalg.norm(np.indices(self.map_dim).T - np.array(bmu), axis=2) ** 2 / (2 * sigma ** 2))
-#
-#             # Update weights of nodes in the BMU neighbourhood
-#             learning_rate = 0.1 * np.exp(-t / self.iterations)
-#             delta = learning_rate * neighbourhood[:, :, np.newaxis] * (x - self.map)
-#             self.map += delta
-#
-#     def predict(self, data):
-#         distances = np.linalg.norm(self.map - data[:, np.newaxis, np.newaxis], axis=3)
-#         return np.argmin(distances, axis=2)
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(0)
 
-# Parameters
-number_of_inputs = 1000
-N = 10  # N^2 points
+class Kohonen:
+    """
+    Kohonen self-organizing map (SOM) implementation for clustering and visualization of high-dimensional data.
 
-upper_bound_x = 1
-lower_bound_x = -1
-upper_bound_m = 0.1
-lower_bound_m = -0.1
+    Parameters:
+    -----------
+    learning_rate : float, default=0.1
+        The learning rate for updating the weights of the neurons.
+    neurons_amount : list of int, default=[100]
+        The number of neurons in each layer of the SOM.
+    """
 
-alpha_initial = 0.1
-sigma_initial = 0.05
-neighbour_radius_initial = N / 2
+    def __init__(self, learning_rate=0.1, neurons_amount=[100]):
+        self.learning_rate = learning_rate
+        self.neurons_amount = neurons_amount
+        self.data = None
+        self.neurons = []
+        self.radius = max(self.neurons_amount[0], len(self.neurons_amount)) / 2
+        self.lamda = None
 
-T = 300  # number of iterations
-t = 1
+    def fit(self, data_set, iteration=10000):
+        """
+        Train the SOM on the given data set.
 
-# Initiate input and neural field
-x1 = np.random.uniform(lower_bound_x, upper_bound_x, number_of_inputs)
-x2 = np.random.uniform(lower_bound_x, upper_bound_x, number_of_inputs)
-w1 = np.random.uniform(lower_bound_m, upper_bound_m, (N, N))
-w2 = np.random.uniform(lower_bound_m, upper_bound_m, (N, N))
+        Parameters:
+        -----------
+        data_set : array-like, shape (n_samples, n_features)
+            The input data to be clustered and visualized.
+        iteration : int, default=10000
+            The number of iterations to train the SOM for.
+        """
+        if len(data_set) == 0:
+            raise ValueError("Data set cannot be empty.")
+        self.data = np.array(data_set)
+        self.lamda = iteration / np.log(self.radius)
 
-# Initial figures
-plt.figure(1)
-plt.plot(x1, x2, 'ob')
-plt.gca().set_prop_cycle(None)
-plt.plot(w1, w2, 'or')
-plt.plot(w1, w2, 'r', linewidth=1)
-plt.plot(w1.T, w2.T, 'r', linewidth=1)
-plt.title('t=0')
-plt.pause(0.001)
+        # initializing the neurons with random weights.
+        for layer_size in self.neurons_amount:
+            layer_neurons = []
+            for _ in range(layer_size):
+                weights = np.random.uniform(0, 1, size=len(data_set[0]))
+                layer_neurons.append(weights)
+            self.neurons.append(layer_neurons)
+        self.neurons = np.array(self.neurons)
 
-# Start training
-while t <= T:
-    # Update parameters
-    alpha = alpha_initial * (1 - t / T)
-    sigma = sigma_initial * (1 - t / T)
-    max_neighbour_radius = int(neighbour_radius_initial * (1 - t / T))
+        # starting the fitting process.
+        for i in range(iteration):
+            # selecting random vector from the given data.
+            vec = self.data[random.randint(0, len(self.data) - 1)]
+            # find which neuron is the closest to the vector.
+            nn = self.nearest_neuron(vec)
+            # updating the learning rate and the radius.
+            curr_learning_rate = self.learning_rate * np.exp(-i / self.lamda)
+            curr_radius = self.radius * np.exp(-i / self.lamda)
+            # going over the neurons in each layer and compute how to change each neuron.
+            for layer_idx, layer_neurons in enumerate(self.neurons):
+                for neuron_idx, neuron_weights in enumerate(layer_neurons):
+                    curr_neuron = neuron_weights
+                    d = np.linalg.norm(np.array(nn) - np.array([layer_idx, neuron_idx]))
+                    neighbourhood = np.exp(- (d ** 2) / (2 * (curr_radius ** 2)))
+                    self.neurons[layer_idx][neuron_idx] += curr_learning_rate * neighbourhood * (vec - curr_neuron)
 
-    # Loop over all input values
-    for i in range(number_of_inputs):
-        # Find minimum distance neural unit (winner)
-        e_norm = (x1[i] - w1) ** 2 + (x2[i] - w2) ** 2
-        min_index = np.argmin(e_norm)
-        minj1 = min_index // N
-        minj2 = min_index % N
-        j1_c, j2_c = minj1, minj2  # Winner coordinates
+            # plotting the network to track progress.
+            if (i % 1000 == 0) or i == iteration - 1:
+                if len(self.neurons_amount) == 1:
+                    self.plot1D(i)
+                else:
+                    self.plot2D(i)
 
-        # Update the winning neuron
-        e_factor = np.exp(-((j1_c - j1_c) ** 2 + (j2_c - j1_c) ** 2) / (2 * sigma))
-        w1[j1_c, j2_c] += alpha * (x1[i] - w1[j1_c, j2_c])
-        w2[j1_c, j2_c] += alpha * (x2[i] - w2[j1_c, j2_c])
+    def refit(self, data, iteration=1000):
+        """
+        Refit the SOM on a new data set.
 
-        # Update the neighbour neurons
-        for neighbour_radius in range(1, max_neighbour_radius + 1):
-            jj1, jj2 = j1_c - neighbour_radius, j2_c
-            if jj1 >= 0:
-                e_factor = np.exp(-((j1_c - jj1) ** 2 + (j2_c - jj2) ** 2) / (2 * sigma))
-                w1[jj1, jj2] += alpha * e_factor * (x1[i] - w1[jj1, jj2])
-                w2[jj1, jj2] += alpha * e_factor * (x2[i] - w2[jj1, jj2])
+        Parameters:
+        -----------
+        data : array-like, shape (n_samples, n_features)
+            The new input data to be clustered and visualized.
+        iteration : int, default=1000
+            The number of iterations to train the SOM for.
+        """
+        if len(data) == 0:
+            raise ValueError("Data set cannot be empty.")
+        self.data = np.array(data)
+        self.lamda = iteration / np.log(self.radius)
+        for i in range(iteration):
+            vec = self.data[random.randint(0, len(self.data) - 1)]
+            nn = self.nearest_neuron(vec)
+            curr_learning_rate = self.learning_rate * np.exp(-i / self.lamda)
+            curr_radius = self.radius * np.exp(-i / self.lamda)
 
-            jj1, jj2 = j1_c + neighbour_radius, j2_c
-            if jj1 < N:
-                e_factor = np.exp(-((j1_c - jj1) ** 2 + (j2_c - jj2) ** 2) / (2 * sigma))
-                w1[jj1, jj2] += alpha * e_factor * (x1[i] - w1[jj1, jj2])
-                w2[jj1, jj2] += alpha * e_factor * (x2[i] - w2[jj1, jj2])
+            for layer_idx, layer_neurons in enumerate(self.neurons):
+                for neuron_idx, neuron_weights in enumerate(layer_neurons):
+                    curr_neuron = neuron_weights
+                    d = np.linalg.norm(np.array(nn) - np.array([layer_idx, neuron_idx]))
+                    neighbourhood = np.exp(- (d ** 2) / (2 * (curr_radius ** 2)))
+                    self.neurons[layer_idx][neuron_idx] += curr_learning_rate * neighbourhood * (
+                            vec - curr_neuron)
+            if (i % 1000 == 0) or i == iteration - 1:
+                if len(self.neurons_amount) == 1:
+                    self.plot1D(i)
+                else:
+                    self.plot2D(i)
 
-            jj1, jj2 = j1_c, j2_c - neighbour_radius
-            if jj2 >= 0:
-                e_factor = np.exp(-((j1_c - jj1) ** 2 + (j2_c - jj2) ** 2) / (2 * sigma))
-                w1[jj1, jj2] += alpha * e_factor * (x1[i] - w1[jj1, jj2])
-                w2[jj1, jj2] += alpha * e_factor * (x2[i] - w2[jj1, jj2])
+    def nearest_neuron(self, vec):
+        """
+        Find the index of the neuron closest to the given input vector.
 
-            jj1, jj2 = j1_c, j2_c + neighbour_radius
-            if jj2 < N:
-                e_factor = np.exp(-((j1_c - jj1) ** 2 + (j2_c - jj2) ** 2) / (2 * sigma))
-                w1[jj1, jj2] += alpha * e_factor * (x1[i] - w1[jj1, jj2])
-                w2[jj1, jj2] += alpha * e_factor * (x2[i] - w2[jj1, jj2])
+        Parameters:
+        -----------
+        vec : array-like, shape (n_features,)
+            The input vector to find the closest neuron to.
 
-    t += 1
-    plt.figure(1)
-    plt.plot(x1, x2, 'ob')
-    plt.gca().set_prop_cycle(None)
-    plt.plot(w1, w2, 'or')
-    plt.plot(w1, w2, 'r', linewidth=1)
-    plt.plot(w1.T, w2.T, 'r', linewidth=1)
-    plt.title(f't={t}')
-    plt.pause(0.001)
+        Returns:
+        --------
+        tuple of int
+            The indices of the closest neuron in the format (layer_idx, neuron_idx).
+        """
+        min_dist = np.inf
+        loc = None
+        for layer_idx, layer_neurons in enumerate(self.neurons):
+            for neuron_idx, neuron_weights in enumerate(layer_neurons):
+                curr_dist = np.linalg.norm(vec - neuron_weights)
+                if min_dist > curr_dist:
+                    loc = (layer_idx, neuron_idx)
+                    min_dist = curr_dist
+        return loc
 
-plt.show()
+    def plot1D(self, t):
+        """
+        Plot the 1D SOM and the input data at a given iteration.
+
+        Parameters:
+        -----------
+        t : int
+            The iteration number to plot the SOM at.
+        """
+        xs = []
+        ys = []
+        for layer_neurons in self.neurons:
+            for neuron_weights in layer_neurons:
+                xs.append(neuron_weights[0])
+                ys.append(neuron_weights[1])
+
+        fig, ax = plt.subplots()
+        ax.scatter([xs], [ys], c='r')
+        ax.set_xlim(0, 1)
+        ax.set_ylim(0, 1)
+        ax.plot(xs, ys, 'b-')
+        ax.scatter(self.data[:, 0], self.data[:, 1], alpha=0.3)
+        plt.title("Plot1D Iteration No. " + str(t))
+        plt.show()
+
+    def plot2D(self, t):
+        """
+        Plot the 2D SOM and the input data at a given iteration.
+
+        Parameters:
+        -----------
+        t : int
+            The iteration number to plot the SOM at.
+        """
+        neurons_x = self.neurons[:, :, 0]
+        neurons_y = self.neurons[:, :, 1]
+        fig, ax = plt.subplots()
+        ax.set_xlim(0, 1)
+        ax.set_ylim(0, 1)
+        for i in range(neurons_x.shape[0]):
+            xh = []
+            yh = []
+            xs = []
+            ys = []
+            for j in range(neurons_x.shape[1]):
+                xs.append(neurons_x[i, j])
+                ys.append(neurons_y[i, j])
+                xh.append(neurons_x[j, i])
+                yh.append(neurons_y[j, i])
+            ax.plot(xs, ys, 'r-', markersize=0, linewidth=1)
+            ax.plot(xh, yh, 'r-', markersize=0, linewidth=1)
+        ax.plot(neurons_x, neurons_y, color='b', marker='o', linewidth=0, markersize=3)
+        ax.scatter(self.data[:, 0], self.data[:, 1], c="g", alpha=0.05, s=5)
+        plt.title("Plot2D Iteration No. " + str(t))
+        plt.show()
--- Kohonen2.py
+++ Kohonen2.py
@@ -59,57 +59,120 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def initialize_weights(input_dim, output_dim):
-    return np.random.rand(input_dim, output_dim)
+# def initialize_weights(input_dim, output_dim):
+#     return np.random.rand(input_dim, output_dim)
+#
+#
+# def topological_neighborhood(radius, center, grid):
+#     if radius == 0:
+#         return 0
+#     distance = np.abs(grid - center)
+#     return np.exp(-(distance ** 2) / (2 * (radius ** 2)))
+#
+#
+# def learning_rate_or_radius(lr, iteration):
+#     return lr * 0.9 * (1 - iteration / 1000)
+#
+#
+# def visualize_som(weights, iteration):
+#     plt.figure(figsize=(6, 6))
+#     plt.scatter(weights[0], weights[1])
+#
+#     # Draw lines connecting each neuron and its neighbors
+#     for i in range(weights.shape[1] - 1):
+#         plt.plot(weights[0, i:i + 2], weights[1, i:i + 2], 'r-')
+#
+#     plt.title(f'SOM Weights at Iteration {iteration}')
+#     plt.xlim(0, 1)
+#     plt.ylim(0, 1)
+#     plt.show()
+#
+#
+# def kohonen_som(inputs, output_dim, max_iteration, initial_lr, initial_radius, visualize_interval=10):
+#     input_dim = inputs.shape[1]
+#     weights = initialize_weights(input_dim, output_dim)
+#     radius = initial_radius
+#     # grid = np.arange(output_dim)
+#     lr = initial_lr
+#     for iteration in range(max_iteration):
+#         # update learning rate and radius
+#         lr = learning_rate_or_radius(lr, iteration)
+#         radius = learning_rate_or_radius(radius, iteration)
+#         for input_vector in inputs:
+#             # compute distances
+#             distances = np.sum((weights - input_vector.reshape(input_dim, -1)) ** 2, axis=0)
+#             # find the winner neuron
+#             winner = np.argmin(distances)
+#             # update weights of winner and its neighbors
+#             for idx in range(output_dim):
+#                 neighborhood = topological_neighborhood(radius, winner, idx)
+#                 weights[:, idx] += lr * neighborhood * (input_vector - weights[:, idx])
+#
+#         # Visualize the SOM at specific intervals
+#         if iteration % visualize_interval == 0:
+#             visualize_som(weights, iteration)
+#
+#     return weights
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-def topological_neighborhood(radius, center, grid):
-    if radius == 0:
-        return 0
-    distance = np.abs(grid - center)
-    return np.exp(-(distance ** 2) / (2 * (radius ** 2)))
+class KohonenSOM:
+    def __init__(self, input_dim, output_dim, max_iteration, initial_lr, initial_radius, visualize_interval=10):
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.max_iteration = max_iteration
+        self.initial_lr = initial_lr
+        self.initial_radius = initial_radius
+        self.visualize_interval = visualize_interval
 
+    def initialize_weights(self):
+        return np.random.rand(self.input_dim, self.output_dim)
 
-def learning_rate_or_radius(lr, iteration):
-    return lr * 0.9 * (1 - iteration / 1000)
+    def topological_neighborhood(self, radius, center, grid):
+        if radius == 0:
+            return 0
+        distance = np.abs(grid - center)
+        return np.exp(-(distance ** 2) / (2 * (radius ** 2)))
 
+    def learning_rate_or_radius(self, lr, iteration):
+        return lr * 0.9 * (1 - iteration / 1000)
 
-def visualize_som(weights, iteration):
-    plt.figure(figsize=(6, 6))
-    plt.scatter(weights[0], weights[1])
+    def visualize_som(self, weights, iteration):
+        plt.figure(figsize=(6, 6))
+        plt.scatter(weights[0], weights[1])
 
-    # Draw lines connecting each neuron and its neighbors
-    for i in range(weights.shape[1] - 1):
-        plt.plot(weights[0, i:i + 2], weights[1, i:i + 2], 'r-')
+        # Draw lines connecting each neuron and its neighbors
+        for i in range(weights.shape[1] - 1):
+            plt.plot(weights[0, i:i + 2], weights[1, i:i + 2], 'r-')
 
-    plt.title(f'SOM Weights at Iteration {iteration}')
-    plt.xlim(0, 1)
-    plt.ylim(0, 1)
-    plt.show()
+        plt.title(f'SOM Weights at Iteration {iteration}')
+        plt.xlim(0, 1)
+        plt.ylim(0, 1)
+        plt.show()
 
+    def fit(self, inputs):
+        self.weights = self.initialize_weights()
+        radius = self.initial_radius
+        lr = self.initial_lr
+        for iteration in range(self.max_iteration):
+            # update learning rate and radius
+            lr = self.learning_rate_or_radius(lr, iteration)
+            radius = self.learning_rate_or_radius(radius, iteration)
+            for input_vector in inputs:
+                # compute distances
+                distances = np.sum((self.weights - input_vector.reshape(self.input_dim, -1)) ** 2, axis=0)
+                # find the winner neuron
+                winner = np.argmin(distances)
+                # update weights of winner and its neighbors
+                for idx in range(self.output_dim):
+                    neighborhood = self.topological_neighborhood(radius, winner, idx)
+                    self.weights[:, idx] += lr * neighborhood * (input_vector - self.weights[:, idx])
 
-def kohonen_som(inputs, output_dim, max_iteration, initial_lr, initial_radius, visualize_interval=10):
-    input_dim = inputs.shape[1]
-    weights = initialize_weights(input_dim, output_dim)
-    radius = initial_radius
-    # grid = np.arange(output_dim)
-    lr = initial_lr
-    for iteration in range(max_iteration):
-        # update learning rate and radius
-        lr = learning_rate_or_radius(lr, iteration)
-        radius = learning_rate_or_radius(radius, iteration)
-        for input_vector in inputs:
-            # compute distances
-            distances = np.sum((weights - input_vector.reshape(input_dim, -1)) ** 2, axis=0)
-            # find the winner neuron
-            winner = np.argmin(distances)
-            # update weights of winner and its neighbors
-            for idx in range(output_dim):
-                neighborhood = topological_neighborhood(radius, winner, idx)
-                weights[:, idx] += lr * neighborhood * (input_vector - weights[:, idx])
+            # Visualize the SOM at specific intervals
+            if iteration % self.visualize_interval == 0:
+                self.visualize_som(self.weights, iteration)
 
-        # Visualize the SOM at specific intervals
-        if iteration % visualize_interval == 0:
-            visualize_som(weights, iteration)
+        return self.weights
 
-    return weights
